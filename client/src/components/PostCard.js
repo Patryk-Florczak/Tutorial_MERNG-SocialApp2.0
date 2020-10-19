b45894543c6156aec1a75ebe2b12dbc5c10db716
Patryk Florczak <projecttristis@gmail.com>
2020-10-19 11:51:38 +0200

    [add] single post page

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -5,6 +5,7 @@ import moment from 'moment';
 
 import { AuthContext } from '../context/auth';
 import LikeButton from './LikeButton';
+import DeleteButton from './DeleteButton';
 
 export default function PostCard({
   post: { body, createdAt, id, username, likeCount, commentCount, likes },
@@ -35,16 +36,7 @@ export default function PostCard({
             {commentCount}
           </Label>
         </Button>
-        {user && user.username === username && (
-          <Button
-            as="div"
-            color="red"
-            floated="right"
-            onClick={() => console.log('Delete post')}
-          >
-            <Icon name="trash" style={{ margin: 0 }} />
-          </Button>
-        )}
+        {user && user.username === username && <DeleteButton postId={id} />}
       </Card.Content>
     </Card>
   );
